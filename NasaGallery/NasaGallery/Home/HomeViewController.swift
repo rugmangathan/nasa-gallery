f7492ec3641d1594806ed86f321e033dbd29cd60
Rugmangathan <mkrugmangathan@gmail.com>
2022-08-21 17:36:51 +0530

    HomeViewController, renamed file.

--- NasaGallery/NasaGallery/Home/HomeViewController.swift
+++ NasaGallery/NasaGallery/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  NasaGallery
 //
 //  Created by Rugmangathan on 21/08/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
