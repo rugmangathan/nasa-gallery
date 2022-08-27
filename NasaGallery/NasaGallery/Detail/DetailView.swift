622a6a462f61e595e22925ad52240d0e893ea91c
Rugmangathan <mkrugmangathan@gmail.com>
2022-08-27 21:33:20 +0530

    DetailViewController, updated UI.

---
+++ NasaGallery/NasaGallery/Detail/DetailView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailView.swift
+//  NasaGallery
+//
+//  Created by Rugmangathan on 27/08/22.
+//
+
+import UIKit
+
+class DetailView: UIView {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
+}
--- NasaGallery/NasaGallery/Detail/DetailViewController.swift
+++ NasaGallery/NasaGallery/Detail/DetailViewController.swift
@@ -6,16 +6,14 @@
 //
 
 import UIKit
+import SwiftUI
 
 class DetailViewController: UIViewController {
   @IBOutlet weak var scrollView: UIScrollView!
-  @IBOutlet weak var stackView: UIStackView!
-  @IBOutlet weak var imageView: UIImageView!
-  @IBOutlet weak var dateLabel: UILabel!
-  @IBOutlet weak var descriptionLabel: UILabel!
-  @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var pageControl: UIPageControl!
   var galleries: [Gallery] = []
   var selectedIndex: Int = 0
+  private var detailViews: [DetailView] = []
   private var imageService: ImageServiceApi = {
     ImageService.shared
   }()
@@ -23,22 +21,73 @@ class DetailViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    title = galleries[selectedIndex].title
-    dateLabel.text = galleries[selectedIndex].date
-    imageService.getImage(for: galleries[selectedIndex].url) { [weak self] result in
-      guard let self = self else { return }
-      if case .success(let image) = result {
-        self.imageViewHeightConstraint.constant = self.calculateImageHeight(sourceImage: image.size)
-        self.imageView.image = image
-        self.descriptionLabel.text = self.galleries[self.selectedIndex].explanation
+    scrollView.delegate = self
+    setupViews()
+  }
+
+  @IBAction func infoButtonTapped(_ sender: UIButton) {
+  }
+
+  func setupViews() {
+    detailViews = createDetailViews()
+
+    setupScrollView(detailViews: detailViews)
+    pageControl.numberOfPages = galleries.count
+    pageControl.currentPage = selectedIndex
+  }
+
+  func setupScrollView(detailViews : [DetailView]) {
+    scrollView.contentSize = CGSize(
+      width: view.bounds.width * CGFloat(detailViews.count),
+      height: scrollView.bounds.height
+    )
+    scrollView.isPagingEnabled = true
+    scrollView.contentOffset = CGPoint(x: view.frame.width * CGFloat(selectedIndex), y: 0)
+
+    for index in 0 ..< detailViews.count {
+      detailViews[index].frame = CGRect(
+        x: view.bounds.width * CGFloat(index),
+        y: 0,
+        width: view.bounds.width,
+        height: scrollView.bounds.height
+      )
+      scrollView.addSubview(detailViews[index])
+    }
+  }
+
+  private func createDetailViews() -> [DetailView] {
+    galleries.compactMap { gallery in
+      guard let detailView = Bundle.main
+        .loadNibNamed("DetailView", owner: self)?.first as? DetailView else {
+        return nil
       }
+      imageService.getImage(for: gallery.url) { result in
+        if case .success(let image) = result {
+          detailView.imageViewHeightConstraint.constant = self.calculateImageHeight(sourceImage: image.size)
+          detailView.imageView.image = image
+        }
+      }
+      return detailView
     }
   }
 
   private func calculateImageHeight(sourceImage: CGSize) -> CGFloat {
-    let oldWidth = sourceImage.width
-    let scaleFactor = view.bounds.width / oldWidth
-    let newHeight = sourceImage.height * scaleFactor
-    return newHeight
+      let oldWidth = sourceImage.width
+      let scaleFactor = view.bounds.width / oldWidth
+      let newHeight = sourceImage.height * scaleFactor
+      return newHeight
+  }
+
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    scrollView.subviews.forEach { $0.removeFromSuperview() }
+    setupViews()
+  }
+}
+
+extension DetailViewController: UIScrollViewDelegate {
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let page = Int(round(scrollView.contentOffset.x / view.frame.width))
+    pageControl.currentPage = page
+    selectedIndex = page
   }
 }
